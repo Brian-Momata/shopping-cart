ac8719879c28558a40a87320bf2db98570c98a10
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-12-26 12:37:03 +0300

    Separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,35 @@
-import { useState, useEffect } from 'react';
-import Home from './Home';
+import { Link } from 'react-router-dom';
 import './App.css'
+import Home from './Home';
+import Footer from './Footer';
 
 const NavLinks = () => {
   return (
     <nav className="navigation">
       <ul className="navLinks">
         <li>
-          <a href="#">Home</a>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <a href="#">Shop</a>
+          <Link to='shop'>Shop</Link>
         </li>
       </ul>
     </nav>
   );
 };
 
-const App = () => {
-  const [productsData, setProductData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://fakestoreapi.com/products');
-        if (!response.ok) throw new Error('Server Error.');
-
-        const data = await response.json();
-        setProductData(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
-  
+const App = ({products}) => {  
   return (
-    <div className="home-page">
+    <>
       <header>
         <h1>Welcome to Our Shop</h1>
         <NavLinks />
       </header>
       <div>
-        <Home products={productsData}/>
+        < Home products={products}/>
       </div>
-      <footer>
-        <p>&copy; {new Date().getFullYear()} Our Shop. All Rights Reserved.</p>
-      </footer>
-    </div>
+      < Footer />
+    </>
   );
 };
 
